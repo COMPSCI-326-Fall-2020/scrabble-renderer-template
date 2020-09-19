77b5dbec2a3eee282ae34aaadbd0b9d23a5b84b0
Nicolas van Kempen <nvankemp@gmail.com>
2020-09-18 23:48:37 -0400

    Add strict mode note.

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 import {shuffle} from './shuffle.js';
 
-'use strict';
+// ES6 modules are [actually strict by default](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode#Strict_mode_for_modules)
+// Therefore, you can in fact comment this out or remove it.
+// 'use strict';
 
 export class Game {
     // Include your code from HW#3
