9c3b85197c29eb961b70d85c62085de63c81e0d4
Nicolas van Kempen <nvankemp@gmail.com>
2020-09-18 22:47:19 -0400

    Initial commit.

---
+++ game.js
@@ -0,0 +1,18 @@
+import {shuffle} from "./shuffle.js";
+
+'use strict';
+
+export class Game {
+    // Include your code from HW#3
+    // You can use the solution when released
+
+    /**
+     * This method will take an HTMLElement, which will either be empty or contain the current grid, and render the
+     * board in that element. For example, if we have a `<div id="board"></div>`, this should be called
+     * `game.render(document.getElementById('board'))`.
+     * @param {HTMLElement} element an HTMLElement to render the board into.
+     */
+    render(element) {
+        // TODO
+    }
+};
